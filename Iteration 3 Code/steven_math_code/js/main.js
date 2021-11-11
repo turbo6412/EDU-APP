b5a9e405e6232038b404c77c0184b18fd38b485f
turbo6412 <anthonyvuong@csu.fullerton.edu>
2021-11-10 17:05:04 -0800

    Add Steven and Yousseef Code Iteration 3

--- Iteration 3 Code/steven_math_code/js/main.js
+++ Iteration 3 Code/steven_math_code/js/main.js
@@ -6,13 +6,13 @@ function check_acorn_1(){
             document.getElementById('btn_list_1').style.borderColor = "green";
             document.getElementById('acorn_1_msg').style.color = "green"
             document.getElementById('acorn_1_msg').innerText = "Correct! The tree has 6 acorns.";
-            document.getElementById('acorn_img_1').src="images/acorn_1.png"
+            document.getElementById('acorn_img_1').src="https://i.imgur.com/CnDBbmp.png"
         }
         else if (values[i].checked){
             document.getElementById('btn_list_1').style.borderColor = "red";
             document.getElementById('acorn_1_msg').style.color = "red"
             document.getElementById('acorn_1_msg').innerText = "Look again carefuly. Here are some red arrows to help.";
-            document.getElementById('acorn_img_1').src="images/acorn_1_arrow.png"
+            document.getElementById('acorn_img_1').src="https://i.imgur.com/GrZkGQf.png"
         }
     }
 }
@@ -25,14 +25,14 @@ function check_acorn_2(){
             document.getElementById('btn_list_2').style.borderColor = "green";
             document.getElementById('acorn_2_msg').style.color = "green"
             document.getElementById('acorn_2_msg').innerText = "Correct! The tree now has 4 acorns.";
-            document.getElementById('acorn_img_2').src="images/acorn_2.png"
+            document.getElementById('acorn_img_2').src="https://i.imgur.com/xg1Kz9j.png"
 
         }
         else if (values[i].checked){
             document.getElementById('btn_list_2').style.borderColor = "red";
             document.getElementById('acorn_2_msg').style.color = "red"
             document.getElementById('acorn_2_msg').innerText = "Look again carefuly. Don't count the acorns with the squirrel.";
-            document.getElementById('acorn_img_2').src="images/acorn_2_arrow.png"
+            document.getElementById('acorn_img_2').src="https://i.imgur.com/XLcbhiQ.png"
         }
     }
 }
@@ -45,13 +45,13 @@ function check_bee_1(){
             document.getElementById('btn_list_3').style.borderColor = "green";
             document.getElementById('bee_1_msg').style.color = "green"
             document.getElementById('bee_1_msg').innerText = "Correct! The field has 9 flowers.";
-            document.getElementById('bee_img_1').src="images/bee_1.png"
+            document.getElementById('bee_img_1').src="https://i.imgur.com/YVaXyFA.png"
         }
         else if (values[i].checked){
             document.getElementById('btn_list_3').style.borderColor = "red";
             document.getElementById('bee_1_msg').style.color = "red"
             document.getElementById('bee_1_msg').innerText = "Look again carefuly. Here are some red arrows to help.";
-            document.getElementById('bee_img_1').src="images/bee_1_arrow.png"
+            document.getElementById('bee_img_1').src="https://i.imgur.com/e23fmlF.png"
         }
     }
 }
@@ -64,13 +64,13 @@ function check_bee_2(){
             document.getElementById('btn_list_4').style.borderColor = "green";
             document.getElementById('bee_2_msg').style.color = "green"
             document.getElementById('bee_2_msg').innerText = "Correct! The field has 3 bees.";
-            document.getElementById('bee_img_2').src="images/bee_2.png"
+            document.getElementById('bee_img_2').src="https://i.imgur.com/hmdxJW3.png"
         }
         else if (values[i].checked){
             document.getElementById('btn_list_4').style.borderColor = "red";
             document.getElementById('bee_2_msg').style.color = "red"
             document.getElementById('bee_2_msg').innerText = "Look again carefuly. Only count the bees.";
-            document.getElementById('bee_img_2').src="images/bee_2_arrow.png"
+            document.getElementById('bee_img_2').src="https://i.imgur.com/7YZL9Lp.png"
         }
     }
 }
@@ -83,15 +83,77 @@ function check_bee_3(){
             document.getElementById('btn_list_5').style.borderColor = "green";
             document.getElementById('bee_3_msg').style.color = "green"
             document.getElementById('bee_3_msg').innerText = "Correct! Every bee will collect honey from 3 flowers.";
-            document.getElementById('bee_img_3').src="images/bee_3.png"
+            document.getElementById('bee_img_3').src="https://i.imgur.com/AET5NFI.png"
         }
         else if (values[i].checked){
             document.getElementById('btn_list_5').style.borderColor = "red";
             document.getElementById('bee_3_msg').style.color = "red"
             document.getElementById('bee_3_msg').innerText = "Look again carefuly. Count the flowers for each bee";
-            document.getElementById('bee_img_3').src="images/bee_3_arrow.png"
+            document.getElementById('bee_img_3').src="https://i.imgur.com/UDdf7Lz.png"
         }
     }
 }
 
-module.exports = {check_acorn_1, check_acorn_2, check_bee_1, check_bee_2, check_bee_3}
+function check_water_1(){
+    var userinput = document.getElementById("water1input")
+
+    if(userinput.value == 4){
+        document.getElementById('btn_list_6').style.borderColor = "green";
+        document.getElementById('water_1_msg').style.color = "green";
+        document.getElementById('water_1_msg').innerText = "Correct! It will take 4 hours for the cup to be full";
+    }
+    else{
+        document.getElementById('btn_list_6').style.borderColor = "red";
+        document.getElementById('water_1_msg').style.color = "red"
+        if(userinput.value < 4)
+            document.getElementById('water_1_msg').innerText = "More than " + userinput.value + "\n Half a cup + Half a cup = Full cup";
+        else if (userinput.value > 4)
+            document.getElementById('water_1_msg').innerText = "Less than " + userinput.value + "\n Half a cup + Half a cup = Full cup";
+        else
+            document.getElementById('water_1_msg').innerText = userinput.value + " is not a valid number!";
+    }
+
+}
+
+function check_water_2(){
+    var userinput = document.getElementById("water2input")
+
+    if(userinput.value == 2){
+        document.getElementById('btn_list_7').style.borderColor = "green";
+        document.getElementById('water_2_msg').style.color = "green";
+        document.getElementById('water_2_msg').innerText = "Correct! It will take 2 hours to fill both cups to half";
+    }
+    else{
+        document.getElementById('btn_list_7').style.borderColor = "red";
+        document.getElementById('water_2_msg').style.color = "red"
+        if(userinput.value < 2)
+            document.getElementById('water_2_msg').innerText = "More than " + userinput.value + "\n They both fill up at the same time";
+        else if(userinput.value > 2)
+            document.getElementById('water_2_msg').innerText = "Less than " + userinput.value + "\n They both fill up at the same time";
+        else
+            document.getElementById('water_2_msg').innerText = userinput.value + " is not a valid number!";
+    }
+
+}
+
+function check_water_3(){
+    var userinput = document.getElementById("water3input")
+
+    if(userinput.value == 8){
+        document.getElementById('btn_list_8').style.borderColor = "green";
+        document.getElementById('water_3_msg').style.color = "green";
+        document.getElementById('water_3_msg').innerText = "Correct! There are 8 ounces of water in both cups";
+    }
+    else{
+        document.getElementById('btn_list_8').style.borderColor = "red";
+        document.getElementById('water_3_msg').style.color = "red"
+        if(userinput.value < 8)
+            document.getElementById('water_3_msg').innerText = "More than " + userinput.value + "\n Add 4 ounces for each cup";
+        else if (userinput.value > 8)
+            document.getElementById('water_3_msg').innerText = "Less than " + userinput.value + "\n Add 4 ounces for each cup";
+        else
+            document.getElementById('water_3_msg').innerText = userinput.value + " is not a valid number!";
+    
+    }
+
+}
