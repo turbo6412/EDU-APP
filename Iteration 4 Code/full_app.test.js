9eff72231b49ea154320a4433b119be5da7173b7
turbo6412 <anthonyvuong@csu.fullerton.edu>
2021-12-05 01:07:12 -0800

    Add unit tests to full app

--- Iteration 4 Code/full_app.test.js
+++ Iteration 4 Code/full_app.test.js
@@ -21,4 +21,34 @@
         displayHomepage()
      ).toBe(null)
  })
- 
+ /* 
+    jest-enviroment
+*/
+const math_temp = require("./main.js")
+
+test('checks for volcano 1', () => {
+    expect(math_temp.show_volcano()).toBe(null)
+});
+
+test('checks for volcano 2', () => {
+    expect(math_temp.volcano_check2('s')).toBe(null)
+});
+
+test('checks for volcano 3', () => {
+    expect(math_temp.volcano_check3('c')).toBe(null)
+});/* 
+    jest-enviroment
+*/
+const math_temp = require("./main.js")
+
+test('checks for volcano 1', () => {
+    expect(math_temp.show_volcano()).toBe(null)
+});
+
+test('checks for volcano 2', () => {
+    expect(math_temp.volcano_check2('s')).toBe(null)
+});
+
+test('checks for volcano 3', () => {
+    expect(math_temp.volcano_check3('c')).toBe(null)
+});
